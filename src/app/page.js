60ae5a95c211e27a9088e12298713196bb542164
Sumit Gupta <sumitguptatiger96@gmail.com>
2025-10-01 22:16:44 +0530

    formated

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
   <>
-  <h1 className="p-4 justify-center items-center">Home Page</h1></>
+  <h1 className="p-4 justify-center items-center">Home Page</h1>
+  </>
   );
 }
